fb66e40e392728c148ab4d6ceb096fce98fc2fad
Karythrys <karythrys@gmail.com>
2025-10-24 14:46:42 -0600

    Attempting to resolve deployment issues by inlining js.

--- src/main.ts
+++ src/main.ts
@@ -9,25 +9,27 @@ import smork from "/smork.png?url";
 import orange from "/orange.svg?url";
 import dragneghost from "/dragneghost.webp?url";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-    <div style="display: none;" >
-        <img id="longnehead" src=${longnehead} >
-        <img id="longnelegs" src=${longnelegs} >
-        <img id="longneneck" src=${longneneck} >
-        <img id="longnetail" src=${longnetail} >
-        <img id="longneturn" src=${longneturn} >
-        <img id="smork" src=${smork} >
-        <img id="orange" src=${orange} >
-        <img id="dragneghost" src=${dragneghost} >
-    </div>
-    <div style="border: 1px solid black; background-color: teal; padding: 8px; display: flex; flex-direction: row; justify-content: space-between;" >
-        <div style="display: flex; flex-direction: column; align-items: start;" >
-            <div>Oranges eaten: <span id="eaten">0</span></div>
-            <div>WASD or arrow keys to navigate.</div>
+window.addEventListener("load", () => {
+    document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+        <div style="display: none;" >
+            <img id="longnehead" src=${longnehead} >
+            <img id="longnelegs" src=${longnelegs} >
+            <img id="longneneck" src=${longneneck} >
+            <img id="longnetail" src=${longnetail} >
+            <img id="longneturn" src=${longneturn} >
+            <img id="smork" src=${smork} >
+            <img id="orange" src=${orange} >
+            <img id="dragneghost" src=${dragneghost} >
         </div>
-        <button id="restart">Restart</button>
-    </div>
-    <canvas id="dragne-game" width="520" height="520" style="border: 1px solid blue" ></canvas>
-`;
+        <div style="border: 1px solid black; background-color: teal; padding: 8px; display: flex; flex-direction: row; justify-content: space-between;" >
+            <div style="display: flex; flex-direction: column; align-items: start;" >
+                <div>Oranges eaten: <span id="eaten">0</span></div>
+                <div>WASD or arrow keys to navigate.</div>
+            </div>
+            <button id="restart">Restart</button>
+        </div>
+        <canvas id="dragne-game" width="520" height="520" style="border: 1px solid blue" ></canvas>
+    `;
 
-run(document.querySelector<HTMLCanvasElement>("#dragne-game")!);
+    run(document.querySelector<HTMLCanvasElement>("#dragne-game")!);
+});    
